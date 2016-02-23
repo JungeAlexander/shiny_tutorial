1447ab6bb2afb563ae96bb667608ae49dc1cec5f
Alexander Junge <ajunge@rth.dk>
2016-02-23 21:55:29 +0100

    app 4: exercise regarding widgets

--- App-4/ui.R
+++ App-4/ui.R
@@ -1,8 +1,14 @@
 
 shinyUI(fluidPage(
-  titlePanel("My Shiny App"),
+  titlePanel("consensusVis"),
   sidebarLayout(
     sidebarPanel(
+      "Create demographic maps with information from the 2010 US Census.",
+      selectInput('select', label = 'Choose a variable to display',
+                  choices = list('Percent White' = 1,
+                                 'Percent Black' = 2)),
+      sliderInput('slider', label = 'Range of interest:',
+                  min = 0, max = 100, value = c(0, 100))
     ),
     mainPanel()
   )
